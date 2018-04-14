b6847e3e7d6732109675904619227465dfc3291e
Jonathan Ansley <ansley.jonathan.e@gmail.com>
2018-04-14 09:46:00 -0400

    inquire accepts input

--- bowling_score.js
+++ bowling_score.js
@@ -1,29 +1,59 @@
+// score mgt system
+
+// Require packages
 const inquirer = require("inquirer");
 
+//
 // functions
 //
-function askForInputString() {
-  console.log('This is the askForInputString() function.');
-  inquirer
-    .prompt(
-      [
 
-        {
-          inputString: "item",
-          type: "input",
-          message: "Please enter the score of the bowling game. e.g. X-X-X-X-X-X-X-X-X-X-XX would be a perfect game."
-        }
+// Get score of bowling game.
+function askForInputString
+{
+  //
+  // function askForInputString() {
+  //   console.log('This is the askForInputString() function.');
+  //   inquirer
+  //     .prompt(
+  //       [
+  //
+  //         {
+  //           inputString: "item",
+  //           type: "input",
+  //           message: "Please enter the score of the bowling game. e.g. X-X-X-X-X-X-X-X-X-X-XX would be a perfect game."
+  //         }
+  //
+  //     ]
+  //
+  //     )
+    //   .then(function(answer) {
+    //     console.log('ID of the product you would like to buy answer.input: ' + answer);
+    //   }
+    // );
+  // }; // end of function askForInputString()
+}
+inquirer.prompt([
 
-    ]
+  {
+    type: "input",
+    name: "rawScore",
+    message: "Please enter the score of the bowling game. e.g. X-X-X-X-X-X-X-X-X-X-XX would be a perfect game.",
+  }
 
-    )
-  //   .then(function(answer) {
-  //     console.log('ID of the product you would like to buy answer.input: ' + answer);
-  //   }
-  // );
-}; // end of function askForInputString()
+  // {
+  //     message: "What's your first name?",
+  //     type: "input",
+  //     name: "firstName"
+  // }
+  //
 
-//////////
+]).then(function(user) {
+    console.log("The rawScore is: " + user.rawScore);
+  //   console.log("");
+    //   // console.log("Just put down the " + user.carryingWhat.join(" and ") + ". It's kind ")
+  // }
+
+}); // end of function getScore()
 
 // storeInArray();
 
@@ -68,29 +98,29 @@ function askForInputString() {
 // loop through scores array
 //
 // {
-var score = 10 + 10 + 10;
+// var score = 10 + 10 + 10;
 // }
 //
 // ///////////////
 //
-function displayScore()
-{
-  console.log('This is the displayScore function.');
-  console.log( 'The score is: ' + score);
-} // end of function displayScore()
+// function displayScore()
+// {
+//   console.log('This is the displayScore function.');
+//   console.log( 'The score is: ' + score);
+// } // end of function displayScore()
 
 /////////
 //
 //
+//
+// function askUserToContinue() {
+//     console.log('This is the askUserToContinue function.');
+//     console.log( 'Would you like to calculate another score?' );
+// } // end of function askUserToContinue()
 
-function askUserToContinue() {
-    console.log('This is the askUserToContinue function.');
-    console.log( 'Would you like to calculate another score?' );
-} // end of function askUserToContinue()
+// askForInputString();
 
-askForInputString();
-
-displayScore();
+// displayScore();
 
 /////
 
@@ -106,8 +136,8 @@ displayScore();
 
 // Call functions
 
-askForInputString();
+// askForInputString();
 
 // storeInArray();
 
-askUserToContinue();
+//askUserToContinue
